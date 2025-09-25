634efe4fc4a323f6b52bfa3c148ef37a1f1a68af
Naoise Law <naoiselawl23@gmail.com>
2025-09-25 16:11:50 +0100

    updates for performace

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "cdn.magicui.design" },
+      { protocol: "https", hostname: "cdn.llm.report" },
+      { protocol: "https", hostname: "pub-83c5db439b40468498f97946200806f7.r2.dev" },
+    ],
+  },
 };
 
 export default nextConfig;
